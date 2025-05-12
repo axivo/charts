e878f9fc7d03266c440dab3762edf7c3ca827d69
Floren Munteanu <19806136+fmunteanu@users.noreply.github.com>
2025-05-12 18:21:06 -0400

    fix: chart release

--- .github/scripts/chart.js
+++ .github/scripts/chart.js
@@ -101,7 +101,7 @@ async function _lintCharts({ core, exec, charts }) {
       return true;
     }
     // Related to extraArgs, see https://github.com/helm/helm/issues/10392
-    // const extraArgs = ['--helm-lint-extra-args', '--skip-schema-validation'];
+    const extraArgs = ['--skip-schema-validation'];
     await exec.exec('ct', ['lint', '--skip-helm-dependencies', '--charts', chartDirs.join(',')]);
     const word = chartDirs.length === 1 ? 'chart' : 'charts';
     core.info(`Successfully linted ${chartDirs.length} ${word}`);
