10cd0e5fac2edcec5445813d5e5c1fc1b70da8fa
Floren Munteanu <19806136+fmunteanu@users.noreply.github.com>
2025-05-13 17:33:05 -0400

    fix: chart release

--- .github/scripts/github-api.js
+++ .github/scripts/github-api.js
@@ -93,12 +93,15 @@ async function _getLastReleaseDate({ github, context, core, chartName }) {
  * @returns {Promise<Array>} - Array of version objects with id and version properties
  */
 async function _getOciPackageVersionIds({ github, context, core, package }) {
-  const packageName = [package.type, package.name].join('/');
+  const packageName = [context.repo.repo, package.type, package.name].join('/');
   core.info(`Searching for all versions of '${packageName}' package...`);
   const repoType = await _getRepositoryType({ github, core, owner: context.repo.owner });
   const isOrg = repoType === 'organization';
   const perPage = 100;
   let versionIds = [];
+  core.info(`DEBUG: repoType is ${repoType}`);
+  core.info(`DEBUG: isOrg is ${isOrg}`);
+  core.info(`DEBUG: Owner is ${context.repo.owner}`);
   try {
     while (true) {
       const { data } = await github.rest.packages[isOrg
@@ -507,7 +510,7 @@ async function createSignedCommit({ github, context, core, git }) {
  * @returns {Promise<boolean>} - True if at least one package version was deleted successfully, false otherwise
  */
 async function deleteOciPackage({ github, context, core, package }) {
-  const packageName = [package.type, package.name].join('/');
+  const packageName = [context.repo.repo, package.type, package.name].join('/');
   try {
     core.info(`Deleting '${packageName}' OCI package...`);
     const versionIds = await _getOciPackageVersionIds({ github, context, core, package });
